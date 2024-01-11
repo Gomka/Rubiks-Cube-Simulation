1edb8ca49685b0c1e342e42d8286259018f3d93c
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2024-01-11 18:40:32 +0100

    Starting with cube face recognition

---
+++ Rubiks Cube/Assets/Scripts/CubeMap.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeMap : MonoBehaviour
+{
+
+    CubeState cubeState;
+
+    public Transform up, left, front, right, back, down;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Set()
+    {
+        cubeState = FindObjectOfType<CubeState>();
+
+        UpdateMap(cubeState.up, up);
+        UpdateMap(cubeState.left, left);
+        UpdateMap(cubeState.front, front);
+        UpdateMap(cubeState.right, right);
+        UpdateMap(cubeState.back, back);
+        UpdateMap(cubeState.down, down);
+    }
+
+    void UpdateMap(List<GameObject> face, Transform side) 
+    {
+        int i = 0;
+        foreach (Transform map in side) 
+        {
+            switch (face[i].name[0])
+            {
+                case 'U': map.GetComponent<Image>().color = Color.white; break;
+                case 'L': map.GetComponent<Image>().color = new Color(1, 0.5f, 0, 1); break;
+                case 'F': map.GetComponent<Image>().color = new Color(0, 1, 0.2f, 1); break;
+                case 'R': map.GetComponent<Image>().color = new Color(1, 0, 0, 1); break;
+                case 'B': map.GetComponent<Image>().color = new Color(0, 0.33f, 1, 1); break;
+                case 'D': map.GetComponent<Image>().color = new Color(0.95f, 1, 0, 1); break;
+                default: break;
+            }
+            i++;
+        }
+    }
+}
---
+++ Rubiks Cube/Assets/Scripts/ReadCube.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadCube : MonoBehaviour
+{
+    [SerializeField] public Transform tUp, tLeft, tFront, tRight, tBack, tDown;
+
+    private int layerMask = 1 << 6; // layer of the cube faces
+    CubeState cubeState;
+    CubeMap cubeMap;
+    public GameObject emptyGO;
+
+    private List<GameObject> upRays = new List<GameObject>();
+    private List<GameObject> leftRays = new List<GameObject>();
+    private List<GameObject> frontRays = new List<GameObject>();
+    private List<GameObject> rightRays = new List<GameObject>();
+    private List<GameObject> backRays = new List<GameObject>();
+    private List<GameObject> downRays = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetRayTransforms();
+        cubeState = FindObjectOfType<CubeState>();
+        cubeMap = FindObjectOfType<CubeMap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReadState();
+    }
+
+    public void ReadState() 
+    {
+        cubeState = FindObjectOfType<CubeState>();
+        cubeMap = FindObjectOfType<CubeMap>();
+
+        cubeState.up = ReadFace(upRays, tUp);
+        cubeState.left = ReadFace(leftRays, tLeft);
+        cubeState.front = ReadFace(frontRays, tFront);
+        cubeState.right = ReadFace(rightRays, tRight);
+        cubeState.back = ReadFace(backRays, tBack);
+        cubeState.down = ReadFace(downRays, tDown);
+
+        cubeMap.Set();
+    }
+
+    void SetRayTransforms() 
+    {
+        upRays = BuildRays(tUp, new Vector3(90, 90, 0));
+        leftRays = BuildRays(tLeft, new Vector3(0, 180, 0));
+        frontRays = BuildRays(tFront, new Vector3(0, 90, 0));
+        rightRays = BuildRays(tRight, new Vector3(0, 0, 0));
+        backRays = BuildRays(tBack, new Vector3(0, 270, 0));
+        downRays = BuildRays(tDown, new Vector3(270, 90, 0));
+    }
+
+    List<GameObject> BuildRays(Transform rayTrasnform, Vector3 direction) 
+    {
+        int rayCount = 0;
+        List<GameObject> rays = new List<GameObject>();
+
+        // 9 rays each facing one side of the cube in the following order:
+        // |0|1|2|
+        // |3|4|5|
+        // |6|7|8|
+
+        for(int y = 1; y > -2; y--) 
+        {
+            for (int x = -1; x < 2; x++)
+            {
+                Vector3 startPos = new Vector3(rayTrasnform.localPosition.x + x, rayTrasnform.localPosition.y + y, rayTrasnform.localPosition.z);
+                GameObject rayStart = Instantiate(emptyGO, startPos, Quaternion.identity, rayTrasnform);
+                rayStart.name = rayCount.ToString();
+                rays.Add(rayStart);
+                rayCount++;
+            }
+        }
+
+        rayTrasnform.localRotation = Quaternion.Euler(direction);
+        return rays;
+    }
+
+    public List<GameObject> ReadFace(List<GameObject> rayStarts, Transform rayTrasnform)
+    {
+        List<GameObject> facesHit = new List<GameObject>();
+
+        foreach(GameObject rayStart in rayStarts)
+        {
+            Vector3 ray = tFront.transform.position;
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, rayTrasnform.forward, out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.DrawRay(ray, rayTrasnform.forward * hit.distance, Color.yellow);
+                facesHit.Add(hit.collider.gameObject);
+            }
+            else 
+            {
+                Debug.DrawRay(ray, rayTrasnform.forward * 1000, Color.green);
+            }
+        }
+
+        return facesHit;
+    }
+}
